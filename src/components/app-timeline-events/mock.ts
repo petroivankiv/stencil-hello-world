9aafe0ec2429d85aabf4fc2d2d2c36e17279ebdd
Petro Ivankiv <petro.ivankiv88@gmail.com>
2021-02-10 11:16:29 +0200

    chore: used evenst data for scatterplot

---
+++ src/components/app-timeline-events/mock.ts
@@ -0,0 +1,137 @@
+export const data = [
+  { date: '2020-01-01', value: 1000 },
+  { date: '2020-02-01', value: 1000 },
+  { date: '2020-03-01', value: 1000 },
+  { date: '2020-04-01', value: 1000 },
+  { date: '2020-05-01', value: 1000 },
+  { date: '2020-06-01', value: 1000 },
+  { date: '2020-07-01', value: 1000 },
+  { date: '2020-08-01', value: 1000 },
+  { date: '2020-09-01', value: 1000 },
+  { date: '2020-10-01', value: 1000 },
+  { date: '2020-11-01', value: 1000 },
+  { date: '2020-12-01', value: 1000 },
+];
+
+export const scatterPlot = [
+  { date: '2020-01-01', value: 1000 },
+  { date: '2020-01-05', value: 1000 },
+  { date: '2020-01-06', value: 1000 },
+  { date: '2020-01-15', value: 1000 },
+  { date: '2020-01-01', value: 1000 },
+  { date: '2020-02-01', value: 1000 },
+  { date: '2020-03-01', value: 1000 },
+  { date: '2020-03-05', value: 1000 },
+  { date: '2020-03-09', value: 1000 },
+  { date: '2020-03-10', value: 1000 },
+  { date: '2020-03-15', value: 1000 },
+  { date: '2020-04-01', value: 1000 },
+  { date: '2020-05-01', value: 1000 },
+  { date: '2020-05-18', value: 1000 },
+  { date: '2020-05-12', value: 1000 },
+  { date: '2020-05-22', value: 1000 },
+  { date: '2020-05-25', value: 1000 },
+  { date: '2020-05-10', value: 1000 },
+  { date: '2020-05-01', value: 1000 },
+  { date: '2020-05-30', value: 1000 },
+  { date: '2020-05-01', value: 1000 },
+  { date: '2020-05-01', value: 1000 },
+  { date: '2020-06-01', value: 1000 },
+  { date: '2020-07-01', value: 1000 },
+  { date: '2020-08-01', value: 1000 },
+  { date: '2020-08-05', value: 1000 },
+  { date: '2020-08-09', value: 1000 },
+  { date: '2020-08-01', value: 1000 },
+  { date: '2020-08-14', value: 1000 },
+  { date: '2020-09-01', value: 1000 },
+  { date: '2020-10-01', value: 1000 },
+  { date: '2020-11-01', value: 1000 },
+  { date: '2020-12-01', value: 1000 },
+];
+
+export const events = [
+  {
+    category: 'Category A',
+    description: 'Description A',
+    events: [
+      {
+        title: 'Title A1 longgg',
+        date: new Date(2020, 1, 10, 20, 50),
+      },
+      {
+        title: 'Title A2',
+        date: new Date(2020, 5, 10, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category B',
+    description: 'Description B',
+    events: [
+      {
+        title: 'Title B1',
+        date: new Date(2020, 3, 15, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category C',
+    description: 'Description C',
+    events: [
+      {
+        title: 'Title C1',
+        date: new Date(2020, 5, 14, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category D',
+    description: 'Description D',
+    events: [
+      {
+        title: 'Title D1',
+        date: new Date(2020, 1, 10, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category E',
+    description: 'Description E',
+    events: [
+      {
+        title: 'Title E1',
+        date: new Date(2020, 10, 25, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category F',
+    description: 'Description F',
+    events: [
+      {
+        title: 'Title F1',
+        date: new Date(2020, 11, 10, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category G',
+    description: 'Description G',
+    events: [
+      {
+        title: 'Title G1',
+        date: new Date(2020, 1, 10, 20, 50),
+      }
+    ],
+  },
+  {
+    category: 'Category H',
+    description: 'Description H',
+    events: [
+      {
+        title: 'Title H1',
+        date: new Date(2020, 8, 5, 20, 50),
+      }
+    ],
+  },
+];
